600eae3248afc265787b2b3a9b43bb3327cd2336
Iam_fortunatus <amfortunatus@gmail.com>
2022-10-17 23:18:03 +0100

    Printf

--- _printf.c
+++ _printf.c
@@ -1,50 +1,91 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _printf - prints formatted data to stdout
- * @format: string that contains the format to print
- * Return: number of characters written
- */
-int _printf(char *format, ...)
-{
-	int written = 0, (*structype)(char *, va_list);
-	char q[3];
-	va_list pa;
+ * printer - Uses the coresponding function to print.
+ * @formati: Type of element to print.
+ * Return: Function address.
+ **/
 
-	if (format == NULL)
-		return (-1);
-	q[2] = '\0';
-	va_start(pa, format);
-	_putchar(-1);
-	while (format[0])
+int (*printer(char formati))(va_list)
+{
+	type_printer frm[] = {
+	{'c', print_c},
+	{'s', print_s},
+	{'i', print_i},
+	{'d', print_i},
+	{'R', print_rot13},
+	{'b', print_b},
+	{'r', print_r},
+	{'S', print_S},
+	{'p', print_p},
+	{'u', print_u},
+	{'x', print_x},
+	{'X', print_X},
+	{'o', print_o},
+	{'\0', NULL}
+	};
+	int i = 0;
+
+	for (i = 0; frm[i].c; i++)
 	{
-		if (format[0] == '%')
+		if (formati == frm[i].c)
+			break;
+	}
+	return (frm[i].f);
+}
+/**
+ * _printf - a function that produces output according to a format
+ * @format: character string
+ * Return: 0 success
+ */
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int i = 0, p_counter = 0;
+	int (*f)(va_list);
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(arg, format);
+	for (; format && format[i]; i++)
+	{
+		if (format[i] == '%')
 		{
-			structype = driver(format);
-			if (structype)
+			if (format[i + 1] == '%')
 			{
-				q[0] = '%';
-				q[1] = format[1];
-				written += structype(q, pa);
+				_putchar(format[i]);
+			    i++;
+				p_counter++;
 			}
-			else if (format[1] != '\0')
+			else if (format[i + 1] == '\0')
 			{
-				written += _putchar('%');
-				written += _putchar(format[1]);
+				_putchar(format[i]);
+				p_counter++;
+				return (p_counter);
 			}
 			else
 			{
-				written += _putchar('%');
-				break;
-			}
-			format += 2;
+				f = printer(format[i + 1]);
+				if (f != NULL)
+				{
+					p_counter += f(arg);
+					i++;
+				}
+				else
+				{
+					_putchar(format[i]);
+					p_counter++;	
+				}	
+			}	
 		}
 		else
-		{
-			written += _putchar(format[0]);
-			format++;
-		}
+		{	_putchar(format[i]);
+			p_counter++;	
+		}	
 	}
-	_putchar(-2);
-	return (written);
+	va_end(arg);
+	return (p_counter);	
 }
